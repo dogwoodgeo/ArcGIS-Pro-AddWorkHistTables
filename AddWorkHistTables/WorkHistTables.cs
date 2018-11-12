0f62afa9e5ec2963de41794d2746bdbfbe359eb6
Bradley Jones <bjones@dogwoodgeo.com>
2018-11-12 13:46:44 -0600

    Initial commit. Simple add tables tool.

---
+++ AddWorkHistTables/WorkHistTables.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using ArcGIS.Core.CIM;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+
+namespace AddWorkHistTables
+{
+    internal class WorkHistTables : Button
+    {
+        protected override void OnClick()
+        {
+            QueuedTask.Run(() =>
+            {
+                try
+                {
+                    var mapView = MapView.Active.Map;
+                    string urlMH = @"O:\SHARE\405 - INFORMATION SERVICES\GIS_Layers\GISVIEWER.SDE@SQL0.sde\SDE.sewerman.tblV8MHWorkhist";
+                    string urlLines = @"O:\SHARE\405 - INFORMATION SERVICES\GIS_Layers\GISVIEWER.SDE@SQL0.sde\SDE.sewerman.tblV8SewerWorkHist";
+                    Uri mhURI = new Uri(urlMH);
+                    Uri linesURI = new Uri(urlLines);
+
+                    // Check to see if the tables are already added to the map.
+                    IReadOnlyList<StandaloneTable> mhTables = mapView.FindStandaloneTables("Manholes Work History");
+                    IReadOnlyList<StandaloneTable> slTables = mapView.FindStandaloneTables("Sewer Lines Work History");
+                    {
+                        if (mhTables.Count == 0 && slTables.Count == 0)
+                        {
+                            StandaloneTable manholesHistory = StandaloneTableFactory.Instance.CreateStandaloneTable(mhURI, mapView, "Manholes Work History");
+                            SetDisplayField(manholesHistory, "Manholes Work History", "COMPDTTM");
+                            StandaloneTable linesHistory = StandaloneTableFactory.Instance.CreateStandaloneTable(linesURI, mapView, "Sewer Lines Work History");
+                            SetDisplayField(linesHistory, "Sewer Lines Work History", "COMPDTTM");
+                        }
+                        else if (mhTables.Count > 0 && slTables.Count == 0)
+                        {
+                            StandaloneTable linesHistory = StandaloneTableFactory.Instance.CreateStandaloneTable(linesURI, mapView, "Sewer Lines Work History");
+                            SetDisplayField(linesHistory, "Sewer Lines Work History", "COMPDTTM");
+                            MessageBox.Show("'Manholes Work History' table is already present in map.\n\n'Sewer Lines Work History' table has been added", "Warning");
+                        }
+
+                        else if (mhTables.Count == 0 && slTables.Count > 0)
+                        {
+                            StandaloneTable manholesHistory = StandaloneTableFactory.Instance.CreateStandaloneTable(mhURI, mapView, "Manholes Work History");
+                            SetDisplayField(manholesHistory, "Manholes Work History", "COMPDTTM");
+                            MessageBox.Show("'Sewer Lines Work History' table is already present in map.\n\n'Manholes Work History' table has been added", "Warning");
+                        }
+
+                        else if (mhTables.Count > 0 && slTables.Count > 0)
+                        {
+                            MessageBox.Show("Sewer Lines and Manholes Work History tables are already present in map.", "Warning");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    string caption = "Failed to add work history tables!";
+                    string message = "Process failed. \n\nSave and restart ArcGIS Pro and try process again.\n\n" +
+                        $"If problem persist, contact your local GIS nerd.\n\n{ex}";
+
+                    //Using the ArcGIS Pro SDK MessageBox class
+                    MessageBox.Show(message, caption);
+                }
+            });
+        }
+
+
+        // Method to set the DisplayField property.
+        public void SetDisplayField(StandaloneTable saTable, string tableName, string fieldName)
+        {
+            var mapView = MapView.Active.Map;
+
+            QueuedTask.Run(() =>
+            {
+                //Gets a list of the standalone tables in map with the name specified in the method arguments.
+                IReadOnlyList<StandaloneTable> tables = mapView.FindStandaloneTables(tableName);
+                foreach (var table in tables)
+                {
+                    //Gets the list of fields from the stand alone table.
+                    var descriptions = table.GetFieldDescriptions(); 
+                    foreach (var desc in descriptions)
+                    {
+                        if (desc.Name == fieldName) // If field name equals "COMPDTTM"
+                        {
+                            //Creates variable that's equal to "COMPDTTM"
+                            string displayName = desc.Name;
+
+                            // Get's the CIM definition from the StandaloneTable
+                            var cimTableDefinition = table.GetDefinition() as CIMStandaloneTable;
+
+                            // Set DisplayField property of the cimTableDefinition equall to displayName("COMPDTTM")
+                            cimTableDefinition.DisplayField = displayName;
+                    
+                            // USe the SetDefinition method to apply the modified table definition (cimTableDefninition)
+                            saTable.SetDefinition(cimTableDefinition);
+
+                            // Use to check the result when debugin in break point.
+                            var result = table.GetDefinition().DisplayField;
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,55 @@
+---
+
+---
+
+# **ArcGIS-Pro-AddWorkHistTables**  
+
+Unlike in ArcMap, ArcGIS Pro will **NOT**, as of release **2.2.3**, let you read related data from a relationship class unless the related table is added to the map. This ArcGIS Pro add-in was developed overcome this by adding the **Sewer Lines Work History** and **Manholes Work History** tables and set the **DisplayField** property for each table so users can view rows by completed date in the pop-up.
+
+This tool is also apart of the larger [LRWRA add-in](https://github.com/dogwoodgeo/ArcGIS-Pro-LRWRA) 
+
+**Note**: I am a novice .Net developer, at best.  The add-in works, but feel free to let me know if you see any code that is "wrong" or just plain bad.  
+
+## Getting Started
+
+Clone or download the repository. Open Visual Studio 2015 or 2017, build solution, launch ArcGIS Pro and check the **Add-Ins** tab.
+
+![](assets/2018-11-12_13-23-35.png)
+
+
+
+### Prerequisites
+
+```
+Language:				C#
+Contributor:			Bradley Jones, bjones@dogwoodgeo.com
+Organization:			Little Rock Water Reclamation Authority (LRWRA)
+Org Site: 				https://git.lrwu.com/portal
+Date:					10/31/2018
+ArcGIS Pro:				2.2
+ArcGIS Pro SDK: 		2.2
+Visual Studio: 			2017, 2015
+.NET Target Framework:	4.6.1
+```
+
+### How To Use Add-In
+
+**NOTE:** The tools in this add-in will only work on the layers if they are named "Manholes" and "Sewer Lines." 
+
+1. Build add-in (see **Getting Started**)
+
+2. Launch ArcGIS Pro.
+
+3. Go to **Add-In** tab.
+
+4. Click on the **Add Work History Tables** button,
+
+5. The **Manholes Work History** and **Sewer Lines Work History** tables will be added to the map, if they are not already present. If they are present, you will receive a message reminding you of such.
+
+   ![](assets/2018-11-12_13-25-16.png)
+
+6. Related data can now be viewed in the default pop-up. Use Up/Down arrow keys to scroll through dates.
+
+   ![](assets/2018-11-12_13-26-29.png)
+
+
